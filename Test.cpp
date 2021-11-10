c88117c17cd203dd2790f59fe7c3bf548d57b505
Sandra-Haidar <sandra.haidar@edu.ece.fr>
2021-11-10 15:35:02 +0100

    test

---
+++ Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sandr on 10/11/2021.
+//
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+// Created by sandr on 10/11/2021.
+//
+
+#ifndef CLUEDO_PROJET_TEST_H
+#define CLUEDO_PROJET_TEST_H
+
+
+class Test {
+
+};
+
+
+#endif //CLUEDO_PROJET_TEST_H
